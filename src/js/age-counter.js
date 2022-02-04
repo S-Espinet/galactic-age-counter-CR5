43e38cfdc121a54c76d750dcf94fe6b099e0afda
S. Espinet <s.lou.espinet@gmail.com>
2022-02-04 09:17:57 -0800

    add js for AgeInYears class creation test - pass

---
+++ src/js/age-counter.js
@@ -0,0 +1,8 @@
+export class AgeInYears {
+  constructor() {
+    this.mercuryAge = "mercuryAge";
+    this.venusAge = "venusAge";
+    this.marsAge = "marsAge";
+    this.jupiterAge = "jupiterAge";
+  }
+}
